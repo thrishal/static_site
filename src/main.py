c75c2539279805a6bc8800dc7ad332927c91ffae
thrishal <reddythrishald@google.com>
2024-04-22 16:10:01 -0500

    Html node script and test script created

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 def main():
     print("hello world")
     textnode1 = TextNode("This is 1", "bold", "https://www.youtube.com")
     print(textnode1)
+    htmlnode1 = HTMLNode("div", "hello world", None, {"class": "greeting", "href": "https://boot.dev"})
+    print(htmlnode1.props_to_html())
 
 
 
