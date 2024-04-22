df76f0385f2efc0d9120dd650fcebeeef2aa3026
thrishal <reddythrishald@google.com>
2024-04-22 15:41:15 -0500

    Created, test files

--- src/textnode.py
+++ src/textnode.py
@@ -1,15 +1,24 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
+
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
-    def __eq__(self, TextNode1: object, TextNode2: object) -> bool:
-        if (TextNode1.text == TextNode2.text) and (TextNode1.text_type == TextNode2.text_type) and (TextNode1.url == TextNode2.url):
+    def __eq__(self, TextNode2: object) -> bool:
+        if (self.text == TextNode2.text) and (self.text_type == TextNode2.text_type) and (self.url == TextNode2.url):
             return True
         
     def __repr__(self) -> str:
-        TEXT = self.text.upper()
-        TEXT_TYPE = self.text_type.upper()
-        URL = self.url.upper()
+        TEXT = self.text
+        TEXT_TYPE = self.text_type
+        URL = self.url
         return f"TextNode({TEXT}, {TEXT_TYPE}, {URL})"
